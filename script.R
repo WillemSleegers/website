7d0b7feac3dabf523dd5e0a789359c2815fa50a2
WillemSleegers <w.sleegers@me.com>
2018-04-01 22:05:50 +0200

    Initial commit

---
+++ script.R
@@ -0,0 +1,26 @@
+
+# Setup -------------------------------------------------------------------
+
+# Load packages
+library(blogdown)
+
+# Set options
+options(servr.daemon = TRUE)
+
+# Inspect website ----------------------------------------------------------
+
+# Build website
+build_site()
+
+# Serve website
+serve_site()
+
+# Add post ----------------------------------------------------------------
+
+blog_title <- "My LaTeX dissertation template"
+tags <- c("LaTeX", "dissertation")
+new_post(blog_title, tags = tags, subdir = "posts")
+
+# Deploy changes to github --------------------------------------------------
+
+system("bash deploy.sh")
